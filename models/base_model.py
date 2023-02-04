6b9042fa4960079210bc875bbcfd4e0c430282a2
MicoBledsoe <MicoMods@gmail.com>
2023-02-03 20:18:14 -0800

    base model somehow deleted

--- models/base_model.py
+++ models/base_model.py
@@ -1,1 +1,28 @@
-i
+#!/usr/bin/python3
+"""The BaseModel class"""
+import uuid
+from datetime import datetime
+
+class BaseModel:
+    def __init__(self, **kwargs):
+        self.id = str(uuid.uuid4())
+        self.created_at = self.updated_at = datetime.now()
+        for key, value in kwargs.items():
+            if key == "created_at" or key == "updated_at":
+                if type(value) is str:
+                    value = datetime.strptime(value, "%Y-%m-%dT%H:%M:%S.%f")
+            setattr(self, key, value)
+
+    def __str__(self):
+        return "[{}] ({}) {}".format(self.__class__.__name__, self.id, self.__dict__)
+
+    def save(self):
+        self.updated_at = datetime.now()
+
+    def to_dict(self):
+        obj_dict = self.__dict__.copy()
+        obj_dict["__class__"] = self.__class__.__name__
+        obj_dict["created_at"] = self.created_at.isoformat()
+        obj_dict["updated_at"] = self.updated_at.isoformat()
+        return obj_dict
+
