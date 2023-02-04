54fc6abeaa4fef2f72f16aeb35c824b8535ba21e
MicoBledsoe <MicoMods@gmail.com>
2023-02-03 20:32:16 -0800

    *args and **kwargs in the constructor to create a BaseModel instance

--- models/base_model.py
+++ models/base_model.py
@@ -4,14 +4,17 @@ import uuid
 from datetime import datetime
 
 class BaseModel:
-    def __init__(self, **kwargs):
-        self.id = str(uuid.uuid4())
-        self.created_at = self.updated_at = datetime.now()
-        for key, value in kwargs.items():
-            if key == "created_at" or key == "updated_at":
-                if type(value) is str:
-                    value = datetime.strptime(value, "%Y-%m-%dT%H:%M:%S.%f")
-            setattr(self, key, value)
+    def __init__(self, *args, **kwargs):
+         if kwargs:
+            for key, value in kwargs.items():
+                if key == "created_at" or key == "updated_at":
+                    if type(value) is str:
+                        value = datetime.strptime(value, "%Y-%m-%dT%H:%M:%S.%f")
+                if key != "__class__":
+                    setattr(self, key, value)
+        else:
+            self.id = str(uuid.uuid4())
+            self.created_at = self.updated_at = datetime.now()
 
     def __str__(self):
         return "[{}] ({}) {}".format(self.__class__.__name__, self.id, self.__dict__)
