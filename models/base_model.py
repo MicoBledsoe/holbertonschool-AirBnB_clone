db915388c315dc5f161555cb7814e00e9c67e912
MicoBledsoe <MicoMods@gmail.com>
2023-02-03 21:07:28 -0800

    updated to handle the creation of instances from dictionary representations

--- models/base_model.py
+++ models/base_model.py
@@ -7,10 +7,16 @@ class BaseModel:
     def __init__(self, *args, **kwargs):
          if kwargs:
             for key, value in kwargs.items():
-                if key != "__class__":
-                if key == "created_at" or key == "updated_at":
-                        value = datetime.strptime(value, "%Y-%m-%dT%H:%M:%S.%f")
-                    setattr(self, key, value)
+                if key == "created_at":
+                    self.created_at = datetime.strptime(kwargs["created_at"],
+                                                        "%Y-%m-%dT%H:%M:%S.%f")
+                elif key == "updated_at":
+                    self.updated_at = datetime.strptime(kwargs["updated_at"],
+                                                        "%Y-%m-%dT%H:%M:%S.%f")
+                elif key == "__class__":
+                    pass
+                else:
+                    setattr(self, key, val)
         else:
             self.id = str(uuid.uuid4())
             self.created_at = self.updated_at = datetime.now()
