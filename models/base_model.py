af9fb8d4cd07ff9a8688efc7870a3da2341d449e
MicoBledsoe <MicoMods@gmail.com>
2023-02-03 20:53:42 -0800

    init updt

--- models/base_model.py
+++ models/base_model.py
@@ -7,10 +7,9 @@ class BaseModel:
     def __init__(self, *args, **kwargs):
          if kwargs:
             for key, value in kwargs.items():
-                if key == "created_at" or key == "updated_at":
-                    if type(value) is str:
-                        value = datetime.strptime(value, "%Y-%m-%dT%H:%M:%S.%f")
                 if key != "__class__":
+                if key == "created_at" or key == "updated_at":
+                        value = datetime.strptime(value, "%Y-%m-%dT%H:%M:%S.%f")
                     setattr(self, key, value)
         else:
             self.id = str(uuid.uuid4())
