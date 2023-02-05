f773f28e5fdc57c6b9770b2d97a3dea80b45e553
MicoBledsoe <108279441+MicoBledsoe@users.noreply.github.com>
2023-02-05 01:09:21 -0600

    Update console.py

--- console.py
+++ console.py
@@ -1,29 +1,22 @@
 #!/usr/bin/python3
+"""CONSOLE"""
 import cmd
 
 class HBNBCommand(cmd.Cmd):
-    """
-    Class HBNBCommand contains the entry point of the command interpreter
-    """
+    """Class HBNBCommand contains the entry point of the command interpreter"""
     prompt = '(hbnb) '
 
     def do_quit(self, args):
-        """
-        quit command to exit the program
-        """
+        """quit command to exit the program"""
         raise SystemExit
 
     def do_EOF(self, args):
-        """
-        EOF command to exit the program
-        """
+        """EOF command to exit the program"""
         print("Goodbye")
         raise SystemExit
 
     def emptyline(self):
-        """
-        Empty line or ENTER entered by user should not execute anything
-        """
+        """Empty line or ENTER entered by user should not execute anything"""
         pass
 
 if __name__ == '__main__':
