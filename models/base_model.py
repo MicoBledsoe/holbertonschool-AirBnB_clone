5b1cb15cae63c74337b9307e6836f5a7f3ff8f5d
JasonBeagle <skipskipperson385@gmail.com>
2023-02-07 15:26:40 -0800

    corrections to base model and place

--- models/base_model.py
+++ models/base_model.py
@@ -1,5 +1,12 @@
 #!/usr/bin/python3
-"""The BaseModel class"""
+"""
+    Project 0x00. AirBnB clone - The console
+    The BaseModel class
+        Methods:
+            __str__: returns a string rep of instance
+            save: updates attributes
+            to_dict: returns dict of key values
+"""
 import uuid
 import models
 from datetime import datetime
@@ -12,6 +19,8 @@ class BaseModel:
             for key, value in kwargs.items():
                 if key != "__class__":
                     setattr(self, key, value)
+                    created_at = ""
+                    updated_at = ""
             s = '%Y-%m-%dT%H:%M:%S.%f'
             self.created_at = datetime.strptime(self.created_at, s)
             self.updated_at = datetime.strptime(self.updated_at, s)
@@ -21,10 +30,9 @@ class BaseModel:
             self.updated_at = self.created_at
             models.storage.new(self)
 
-
     def __str__(self):
         """returns string representation of BaseModel instance"""
-        return "[{}] ({}) {}".format(type(self).__name__, self.id, self.__dict__)
+        return f"[{type(self).__name__}] ({self.id}) {self.__dict__}"
 
     def save(self):
         """updates attribute updated_at with current datetime"""
@@ -34,7 +42,7 @@ class BaseModel:
     def to_dict(self):
         """DICTIONARY"""
         obj_dict = self.__dict__.copy()
-        obj_dict["__class__"] = type(self).__name__
+        obj_dict["__class__"] = self.__class__.__name__
         obj_dict["created_at"] = obj_dict["created_at"].isoformat()
         obj_dict["updated_at"] = obj_dict["updated_at"].isoformat()
         return obj_dict
